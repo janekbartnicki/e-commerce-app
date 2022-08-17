d1c1e53543d82c3974706e501a8a59542f02c397
janekbartnicki <jachu01@poczta.onet.pl>
2022-08-17 10:49:20 +0200

    1

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const express = require('express');
 const app = express();
-app.get('/', ())
+
+app.get('/', (req, res) => {
+    res.send(`
+        <form method="POST">
+            <input type="text" name="email" id="" placeholder="E-mail">
+            <input type="text" name="password" id="" placeholder="Password">
+            <input type="text" name="passwordConfirmation" id="" placeholder="Confirm password">
+            <button>Sign Up</button>
+        </form>
+    `);
+})
+
+app.post('/', (req, res) => {
+    console.log(req);
+})
+
+app.listen(3000)
