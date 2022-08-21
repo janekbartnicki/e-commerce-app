6e71ae50496352b6c3ecdf55387b53a92fc81744
janekbartnicki <jachu01@poczta.onet.pl>
2022-08-21 14:08:55 +0200

    1

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.send(`
@@ -12,20 +16,8 @@ app.get('/', (req, res) => {
     `);
 })
 
-const bodyParser = (req, res, next) => {
-    if(req.method === 'POST') {
-        req.on('data', data => {
-            const dataRecived = {};
-            for(let element of data.toString('utf8').split('&')) {
-                dataRecived[element.split('=')[0]] = element.split('=')[1];
-            }
-    
-            console.log(dataRecived);
-        });
-    }
-}
-
 app.post('/', (req, res) => {
+    console.log(req.body)
 })
 
 app.listen(3000)
